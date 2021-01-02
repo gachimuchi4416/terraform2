7b3dd3c5fa0029e2ac2eb909e84f6d9f8201ee43
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-02 18:08:15 +0000

    プライベートサブネットの作成

--- vpc.tf
+++ vpc.tf
@@ -28,21 +28,44 @@ resource "aws_internet_gateway" "walter-igw" {
   }
 }
 
-#ルートテーブルの定義
+#パブリックルートテーブルの定義
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.walter.id
   tags = {
     Name = "walter-public-rt"
   }
 }
-#ルートの定義
+#パブリックルートの定義
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   gateway_id             = aws_internet_gateway.walter-igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
-#ルートテーブルの関連付け
+#パブリックルートテーブルの関連付け
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
+}
+
+#プライベートサブネットを作成
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.walter.id
+  cidr_block              = "10.0.64.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "ap-northeast-1a"
+
+  tags = {
+    Name = "walter-private-subnet-1a"
+  }
+}
+
+#プライベートトルートの定義
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.walter.id
+}
+
+#プライベートルートテーブルの関連付け
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
