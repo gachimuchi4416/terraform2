e49fe643dc5bcd16ff617fb4ee90479714b18271
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-02 07:20:23 +0000

    main.tfファイルを作成

--- main.tf
+++ main.tf
@@ -1,45 +1,12 @@
-locals {
-  walter_instance_type = "t3.micro"
+module "web_server" {
+  source        = "./http_server"
+  instance_type = "t3.micro"
 }
 
-resource "aws_instance" "walter" {
-  ami                    = "ami-01748a72bed07727c"
-  instance_type          = local.walter_instance_type
-  vpc_security_group_ids = [aws_security_group.walter_ec2.id]
-  tags = {
-    Name = "walter"
-  }
-
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-  EOF
-}
-
-output "walter_public_dns" {
-  value = aws_instance.walter.public_dns
+output "public_dns" {
+  value = module.web_server.public_dns
 }
 
 provider "aws" {
   region = "ap-northeast-1"
-}
-
-#EC2向けセキュリティグループの定義
-resource "aws_security_group" "walter_ec2" {
-  name = "walter-ec2"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
