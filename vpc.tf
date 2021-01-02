27585f577227b631c713d006f3f46d5e1ce5a91c
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-02 17:30:17 +0000

    パブリックサブネットを作成

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "walter" {
     Name = "walter-vpc"
   }
 }
+
+#パブリックサブネットを作成
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.walter.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+
+  tags = {
+    Name = "walter-public-subnet-1a"
+  }
+}
