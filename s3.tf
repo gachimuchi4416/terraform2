265ec4dbd158b50362e1cc85b4be127112bc0882
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-02 17:15:43 +0000

    ALBログバケットを作成

--- s3.tf
+++ s3.tf
@@ -1,6 +1,7 @@
 #S3プライベートバケットの定義
 resource "aws_s3_bucket" "private-walter" {
   bucket = "gachimuchi-terraform20210102"
+  force_destroy = true
 
   versioning {
     enabled = true
@@ -25,3 +26,47 @@ resource "aws_s3_bucket_public_access_block" "private-walter" {
 }
 
 #S3パブリックバケットの定義
+resource "aws_s3_bucket" "public-walter" {
+  bucket = "public-gachimuchi-terraform20210102"
+  acl    = "public-read"
+  force_destroy = true
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+#ALBのログバケット
+resource "aws_s3_bucket" "alb_log" {
+  bucket        = "alb-log-gachimuchi-terraform20210102"
+  force_destroy = true
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
