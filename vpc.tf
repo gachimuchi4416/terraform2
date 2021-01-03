de9f91d08d229646e58688b3d20f667d8aad6212
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-03 08:47:15 +0000

    サブネット、ルートテーブル、NATゲートウェイ(それに付随するEIP)のマルチAZ化

--- vpc.tf
+++ vpc.tf
@@ -8,12 +8,24 @@ resource "aws_vpc" "walter" {
   }
 }
 
-#パブリックサブネットを作成
-resource "aws_subnet" "public" {
+#パブリックサブネット1aを作成
+resource "aws_subnet" "public_0" {
   vpc_id                  = aws_vpc.walter.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-northeast-1a"
+
+  tags = {
+    Name = "walter-public-subnet-1a"
+  }
+}
+
+#パブリックサブネット1acを作成
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.walter.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
 
   tags = {
     Name = "walter-public-subnet-1a"
@@ -41,16 +53,22 @@ resource "aws_route" "public" {
   gateway_id             = aws_internet_gateway.walter-igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
-#パブリックルートテーブルの関連付け
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+#パブリックルートテーブルの関連付け(1a)
+resource "aws_route_table_association" "public_0" {
+  subnet_id      = aws_subnet.public_0.id
   route_table_id = aws_route_table.public.id
 }
 
-#プライベートサブネットを作成
-resource "aws_subnet" "private" {
+#パブリックルートテーブルの関連付け(1a)
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+#プライベートサブネット1aを作成
+resource "aws_subnet" "private_0" {
   vpc_id                  = aws_vpc.walter.id
-  cidr_block              = "10.0.64.0/24"
+  cidr_block              = "10.0.65.0/24"
   map_public_ip_on_launch = false
   availability_zone       = "ap-northeast-1a"
 
@@ -59,32 +77,87 @@ resource "aws_subnet" "private" {
   }
 }
 
-#プライベートトルートの定義
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.walter.id
+#プライベートサブネット1cを作成
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.walter.id
+  cidr_block              = "10.0.66.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "ap-northeast-1c"
+
   tags = {
-    Name = "walter-private-rt"
+    Name = "walter-private-subnet-1c"
   }
 }
 
-#プライベートルートテーブルの関連付け
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+#プライベートトルートの定義(1a)
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.walter.id
+  tags = {
+    Name = "walter-private-rt-1a"
+  }
 }
 
-#EIPの定義
-resource "aws_eip" "nat_gataway" {
+#プライベートトルートの定義(1c)
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.walter.id
+  tags = {
+    Name = "walter-private-rt-1c"
+  }
+}
+
+#プライベートルートテーブルの関連付け(1a)
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+#プライベートルートテーブルの関連付け(1c)
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+#EIPの定義(1aに配置しているNATゲートウェイ用)
+resource "aws_eip" "nat_gataway_0" {
   vpc        = true
   depends_on = [aws_internet_gateway.walter-igw]
 }
 
-#NATゲートウェイの定義
-resource "aws_nat_gateway" "walter" {
-  allocation_id = aws_eip.nat_gataway.id
-  subnet_id     = aws_subnet.public.id
+#EIPの定義(1cに配置しているNATゲートウェイ用)
+resource "aws_eip" "nat_gataway_1" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.walter-igw]
+}
+
+#NATゲートウェイの定義(1aに配置)
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gataway_0.id
+  subnet_id     = aws_subnet.public_0.id
   depends_on    = [aws_internet_gateway.walter-igw]
   tags = {
-    Name = "walter-nat-gw"
+    Name = "walter-nat-gw-1a"
   }
+}
+
+#NATゲートウェイの定義(1cに配置)
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gataway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.walter-igw]
+  tags = {
+    Name = "walter-nat-gw-1c"
+  }
+}
+
+#プライベートルートの定義(1a)
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+#プライベートルートの定義(1c)
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
 }
