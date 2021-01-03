7cbe3a567063fef14157ff6055e391e2e42f3dfd
gachimuchi4416 <kamibayashi.dev@gmail.com>
2021-01-03 17:46:29 +0000

    Aレコードを定義

--- route53.tf
+++ route53.tf
@@ -8,3 +8,18 @@ resource "aws_route53_zone" "test_walter" {
 }
 
 #ALBのDNSレコードを定義
+resource "aws_route53_record" "walter" {
+  zone_id = aws_route53_zone.walter.zone_id
+  name    = aws_route53_zone.walter.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.walter.dns_name
+    zone_id                = aws_lb.walter.zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "domain_name" {
+  value = aws_route53_record.walter.name
+}
